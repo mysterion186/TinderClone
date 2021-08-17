e3f4aae8ba2a8a765419d233a82f6efdd368e530
Mysterion186 <kowsikan@hotmail.fr>
2021-08-17 18:58:28 +0200

    ajout data users + affichage premiere page

--- App.js
+++ App.js
@@ -1,112 +1,45 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from 'react'
+import {Text,View,Image,StyleSheet} from 'react-native'
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App =()=>{
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      <View style={styles.pageContainer}>
+        <View style={styles.card}>
+          <Image style={styles.image}
+            source={{uri : "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png"}}
+          />
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </View>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  pageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex : 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  card : {
+    width: '95%',
+    height : '70%',
+    borderRadius: 10,
+
+    shadowColor: '#000',
+    shadowOffset : {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius : 6.68,
+    elevation : 11,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  image : {
+    width : "100%",
+    height : "100%",
+    borderRadius : 10
+  }
+}
+)
 
 export default App;
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # TinderClone
 
-Tuto youtube pour faire un clone de tinder
+Tuto youtube pour faire un clone de tinder [source](https://www.youtube.com/watch?v=n1o9sEiiydo&list=WL&index=2&ab_channel=VadimSavin)
