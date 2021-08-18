69239a581199c17d2493368c80bc71ed3f97bf0d
Mysterion186 <kowsikan@hotmail.fr>
2021-08-18 12:27:06 +0200

    Card par défaut

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import {Text,View,Image,StyleSheet} from 'react-native'
+import {Text,View,Image,StyleSheet,ImageBackground} from 'react-native'
 
 const App =()=>{
   return (
 
       <View style={styles.pageContainer}>
         <View style={styles.card}>
-          <Image style={styles.image}
+          <ImageBackground style={styles.image}
             source={{uri : "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png"}}
-          />
+          >
+            <View style={styles.cardInner}>
+              <Text style={styles.name}>
+                Elon Musk
+              </Text>
+              <Text style={styles.bio}>
+                A dude with a rocket is looking for a gal with fuel
+              </Text>
+            </View>
+          </ImageBackground>
         </View>
       </View>
   );
@@ -37,7 +46,22 @@ const styles = StyleSheet.create({
   image : {
     width : "100%",
     height : "100%",
-    borderRadius : 10
+    borderRadius : 10,
+    overflow : "hidden",
+    justifyContent : 'flex-end'
+  },
+  cardInner :{
+    padding : 10
+  },
+  name : {
+    fontSize : 30,
+    color : 'white',
+    fontWeight : 'bold',
+  },
+  bio : {
+    fontSize : 18,
+    color : 'white',
+    lineHeight : 25,
   }
 }
 )
